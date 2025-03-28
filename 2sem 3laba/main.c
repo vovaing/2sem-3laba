6298ea429c96ca6ceb40e215a5ec9298f8155a6b
vovaing <palakangor@gmail.com>
2025-03-28 18:28:43 +0300

    Initial Commit

---
+++ 2sem 3laba/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  2sem 3laba
+//
+//  Created by Владимир Кюри on 28.03.2025.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
